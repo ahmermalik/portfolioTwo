103d9583898b3ed8dc3c82bac1bb812218e1d0e0
Ahmer Malik <ahmer9800@gmail.com>
2018-05-29 18:21:51 -0500

    update work.js to include correct projects.

--- src/components/Work.js
+++ src/components/Work.js
@@ -10,14 +10,14 @@ class Work extends React.Component {
     <section id='Portfolio'>
         <div className='mw8 mt5 center db ph0-ns ph2'>
         <Title text='Portfolio'/>
-        <WorkTile title='Sish Kabob Cafe' info='Responsive mobile app design' summary=' A React web app for a restaurant in Katy, TX. Features full menu and utilizes Yelp api to display current reviews.' img='images/preview/sishkabob123.png' link='/CrowdScores'/>
         <WorkTile title='Cryptotistics' info='Web app & responsive design' summary='Cryptotistics utilizes multiple APIs in order to deliver real time stats of various cryptocurrencies.' img='images/preview/cryptotistics.png' link='Suggestv'/>
+        <WorkTile title='Crypto Decryptd' info='Responsive mobile app design' summary=' A React web app for a restaurant in Katy, TX. Features full menu and utilizes Yelp api to display current reviews.' img='images/preview/sishkabob123.png' link='/CrowdScores'/>
 
-        <WorkTile title='Insert Project 1' info='Mobile app & atm design' summary='Bitcoin safe and easy. Numilli makes it easy to securely buy and sell bitcoin currency, on the go using a mobile application and ATMs.' img='images/preview/numilli.jpg' link='Numilli'/>
-        <WorkTile title='Insert Project 2' info='Web app design' summary='YunoJuno connects the best freelancers with great companies. Employers work directly with freelancers with YunoJuno managing contact and payment.' img='images/preview/yunojuno.jpg' link='YunoJuno'/>
+        <WorkTile title='Shish Kabob Cafe' info='Mobile app & atm design' summary='Bitcoin safe and easy. Numilli makes it easy to securely buy and sell bitcoin currency, on the go using a mobile application and ATMs.' img='images/preview/numilli.jpg' link='Numilli'/>
+        <WorkTile title='Gas Price App' info='Web app design' summary='YunoJuno connects the best freelancers with great companies. Employers work directly with freelancers with YunoJuno managing contact and payment.' img='images/preview/yunojuno.jpg' link='YunoJuno'/>
 
-        <WorkTileOut title='Insert Project 3' info='Web app - design & development' summary='Web app showing the bitcoin ATM locations - under development' img='images/preview/numilli-site.jpg' link='http://numilli.surge.sh/'/>
-        <WorkTile title='Insert Project 4' info='Mobile app design' summary='Quidco is the number one cashback service. Partnering with retailers to help consumers earn cashback every year.' img='images/preview/quidco.jpg' link='Quidco'/>
+        <WorkTileOut title='Restaurant Reviewer' info='Web app - design & development' summary='Web app showing the bitcoin ATM locations - under development' img='images/preview/numilli-site.jpg' link='http://numilli.surge.sh/'/>
+        <WorkTile title='Chatroom App' info='Mobile app design' summary='Quidco is the number one cashback service. Partnering with retailers to help consumers earn cashback every year.' img='images/preview/quidco.jpg' link='Quidco'/>
 
         <h1 className='fw4'>Non-profit Projects</h1>
         <p className='mw7'>As a developer and a leader, it is important to share your abilities with organizations that may utilize it strategically to improve our world.</p>
