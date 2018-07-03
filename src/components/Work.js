f65b7cc71d09c17033b0f3c65f0b126c272d383c
Ahmer Malik <ahmer9800@gmail.com>
2018-07-03 16:55:13 -0500

    update project descriptions and techs

--- src/components/Work.js
+++ src/components/Work.js
@@ -10,13 +10,13 @@ class Work extends React.Component {
     <section id='Portfolio'>
         <div className='mw8 mt5 center db ph0-ns ph2'>
         <Title text='Portfolio'/>
-        <WorkTileOut title='Cryptotistics' info='Python, Tornado, JavaScript, PostgreSQL, OAuth, jQuery ' summary='Cryptocurrency portfolio web app. Utilizing a complex database, allows multiple users to create realtime portfolios.' img='images/preview/cryptotistics.png' link='https://github.com/ahmermalik/cryptoCurrencyApp'/>
-        <WorkTileOut title='Crypto Analysis' info='Node.js, PostgreSQL, Express, ' summary=' Analyze crypto price movements utilize historic data ' img='/images/preview/comingsoon.png' link='https://github.com/ahmermalik/tools'/>
+        <WorkTileOut title='Cryptotistics' info='Python, Tornado, JavaScript, PostgreSQL, OAuth, jQuery ' summary='Crypto currency portfolio web app. Utilizing a complex database, allows multiple users to create real time portfolios.' img='images/preview/cryptotistics.png' link='https://github.com/ahmermalik/cryptoCurrencyApp'/>
+        <WorkTileOut title='Crypto Analysis' info='Node.js, PostgreSQL, Express ' summary=' Analyze price volatility utilizing historic data. ' img='/images/preview/comingsoon.png' link='https://github.com/ahmermalik/tools'/>
 
-        <WorkTileOut title='Shish Kabob Cafe' info='HTML5, CSS3, jQuery, JavaScript' summary='Web app for a local restaurant.' img='/images/preview/shishkabob-min.png' link='https://github.com/ahmermalik/kabobCafe'/>
-        <WorkTileOut title='Chatroom App' info='Socket.IO, JavaScript, Express, Node.js, HTML5, Handlebars.js' summary='A socket io web app with designated chatrooms.' img='/images/preview/comingsoon.png' link='https://github.com/ahmermalik/chatroomApp'/>
+        <WorkTileOut title='Shish Kabob Cafe' info='JavaScript, Google Analytics, HTML5, CSS3, jQuery' summary='Responsive, mobile friendly web app for a local restaurant.' img='/images/preview/shishkabob-min.png' link='https://github.com/ahmermalik/kabobCafe'/>
+        <WorkTileOut title='Chatroom App' info='Socket.IO, JavaScript, Express, Node.js, HTML5, Handlebars.js' summary='Socket.IO web app with designated chat rooms.' img='/images/preview/comingsoon.png' link='https://github.com/ahmermalik/chatroomApp'/>
 
-        <WorkTileOut title='Weather App' info='Python, PostgreSQL, Handlebars.js' summary='Web application utilize weather api to render data for various cities.' img='/images/preview/weathapp-min.png' link='https://github.com/ahmermalik/weatherApp'/>
+        <WorkTileOut title='Weather App' info='Python, PostgreSQL, Jinja2' summary='Web application utilize weather api to render data for various cities.' img='/images/preview/weathapp-min.png' link='https://github.com/ahmermalik/weatherApp'/>
         <WorkTileOut title='Gas Price App' info='HTML5, CSS3, jQuery, JavaScript' summary='Single page static web app to calculate cost variances which produces automated recommendation of payment channel.' img='/images/preview/gasprice-min.png' link='https://github.com/ahmermalik/ahmermalik.github.io'/>
 
             <h1 className='fw4'>Non-profit Projects</h1>
