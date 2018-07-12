d72618d579a5179e59fbb295eeee04cce8058136
Ahmer Malik <ahmer9800@gmail.com>
2018-07-11 21:00:33 -0500

    update project descriptions

--- src/components/Work.js
+++ src/components/Work.js
@@ -13,11 +13,11 @@ class Work extends React.Component {
         <WorkTileOut title='Cryptotistics' info='Python, Tornado, JavaScript, PostgreSQL, OAuth, jQuery ' summary='Crypto currency portfolio web app. Utilizing a complex database, allows multiple users to create real time portfolios.' img='images/preview/cryptotistics.png' link='https://github.com/ahmermalik/cryptoCurrencyApp'/>
         <WorkTileOut title='Crypto-currency Market Report' info='Node.js, PostgreSQL, Express, Chart.js ' summary='Back end application to analyze price volatility utilizing historic data. ' img='/images/preview/comingsoon.png' link='https://github.com/ahmermalik/tools'/>
 
-        <WorkTileOut title='Shish Kabob Cafe' info='JavaScript, Google Analytics, HTML5, CSS3, jQuery' summary='Created a responsive, mobile friendly web app for a local restaurant, and setup delivery system.' img='/images/preview/shishkabob-min.png' link='https://github.com/ahmermalik/kabobCafe'/>
-        <WorkTileOut title='Chatroom App' info='Socket.IO, JavaScript, Express, Node.js, HTML5, Handlebars.js' summary='Socket.IO web app with designated chat rooms.' img='/images/preview/comingsoon.png' link='https://github.com/ahmermalik/chatroomApp'/>
+        <WorkTileOut title='Shish Kabob Cafe' info='JavaScript, Google Analytics, jQuery' summary='Created a responsive, mobile friendly web app for a local restaurant, and setup delivery system.' img='/images/preview/shishkabob-min.png' link='https://github.com/ahmermalik/kabobCafe'/>
+        <WorkTileOut title='Chatroom App' info='Socket.IO, JavaScript, Express, Node.js, Handlebars.js' summary='Socket.IO web app with designated chat rooms.' img='/images/preview/comingsoon.png' link='https://github.com/ahmermalik/chatroomApp'/>
 
         <WorkTileOut title='Weather App' info='Python, PostgreSQL, Jinja2' summary='Web application utilize weather api to render data for various cities.' img='/images/preview/weathapp-min.png' link='https://github.com/ahmermalik/weatherApp'/>
-        <WorkTileOut title='Gas Price App' info='HTML5, CSS3, jQuery, JavaScript' summary='Single page static web app to calculate cost variances which produces automated recommendation of payment channel.' img='/images/preview/gasprice-min.png' link='https://github.com/ahmermalik/ahmermalik.github.io'/>
+        <WorkTileOut title='Gas Price App' info='JavaScript, jQuery' summary='Single page static web app to calculate cost variances which produces automated recommendation of payment channel.' img='/images/preview/gasprice-min.png' link='https://github.com/ahmermalik/ahmermalik.github.io'/>
 
             <h1 className='fw4'>Non-profit Projects</h1>
             <p className='mw7'> As a developer and leader, I believe it is vital to share resources with individuals and organizations that will carry the world forward.</p>
