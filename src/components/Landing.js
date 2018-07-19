302838ca44720d5b74ead45ea78c2af5a20677a8
Ahmer Malik <ahmer9800@gmail.com>
2018-07-18 20:58:36 -0500

    update location

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,7 +10,7 @@ class Landing extends React.Component {
           <div className="dib v-top w-50-ns w-100 mt4-ns pa0-ns pa3 mw6 tl-ns tc ">
           <h1 className='fw5 f1 mb2'>Hello!</h1>
           <h2 className='fw3' style={{lineHeight: '1.6'}}>
-          I'm a Full Stack Web Developer living in Austin, TX. I previously worked for
+          I'm a Full Stack Web Developer living in Texas. I previously worked for
           <a className='link hover-red' target='blank' href='http://www.emerson.com/en-us'> Emerson Electric</a>.
           </h2>
           <a className="f6 grow no-underline ph4 pv3 mt4 dib bg-black white mt5 shadow-4" style={{borderRadius:'6px'}} href="#form">Get in touch</a>
